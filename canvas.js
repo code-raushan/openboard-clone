07f083e11787e04ab5a707083015646e7d42ff1e
Raushan Kumar <raushan.development@gmail.com>
2023-07-02 17:59:12 +0530

    multicolor stroke feature added

--- canvas.js
+++ canvas.js
@@ -1,28 +1,49 @@
-const canvas = document.querySelector('canvas');
+const canvas = document.querySelector("canvas");
+const pencilColorsElem = document.querySelectorAll(".pencil-color");
 
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
 
-const tool = canvas.getContext('2d');
-tool.strokeStyle = "red";
-tool.lineWidth = "3"
+const tool = canvas.getContext("2d");
+let pencilColor = "blue";
 
-let mousedown = false
+tool.lineWidth = "3";
 
-// move to -> line to 
+let mousedown = false;
 
-canvas.addEventListener('mousedown', (e)=>{
-    mousedown = true;
-    tool.beginPath();
-    tool.moveTo(e.clientX, e.clientY);
+// move to -> line to
+
+pencilColorsElem.forEach((colorElem) => {
+  colorElem.addEventListener("click", () => {
+    pencilColor = colorElem.classList[0];
+  });
 });
-canvas.addEventListener('mousemove', (e)=>{
-    if(mousedown){
-        tool.lineTo(e.clientX, e.clientY);
-        tool.stroke();
-    }
-    
+
+canvas.addEventListener("mousedown", (e) => {
+  mousedown = true;
+  strokeBegin({
+    x: e.clientX,
+    y: e.clientY,
+  });
 });
-canvas.addEventListener('mouseup', ()=>{
-    mousedown = false;
-})
+canvas.addEventListener("mousemove", (e) => {
+  if (mousedown) {
+    drawStroke({
+        x: e.clientX,
+        y: e.clientY
+    })
+  }
+});
+canvas.addEventListener("mouseup", () => {
+  mousedown = false;
+});
+
+function strokeBegin(strokeObj) {
+  tool.beginPath();
+  tool.strokeStyle = pencilColor;
+  tool.moveTo(strokeObj.x, strokeObj.y);
+}
+function drawStroke(strokeObj) {
+  tool.lineTo(strokeObj.x, strokeObj.y);
+  tool.stroke();
+}
