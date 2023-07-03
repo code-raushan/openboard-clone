989b6e8b949518b80c1f484ba8df78384d4353b2
Raushan Kumar <raushan.development@gmail.com>
2023-07-03 17:17:07 +0530

    line width and download feature implemented

--- canvas.js
+++ canvas.js
@@ -1,13 +1,17 @@
 const canvas = document.querySelector("canvas");
 const pencilColorsElem = document.querySelectorAll(".pencil-color");
+const pencilColorsWidth = document.querySelector(".pencilwidth");
+const downloadBtn = document.querySelector("img[alt='download']");
 
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
 
 const tool = canvas.getContext("2d");
+
+tool.lineWidth
+
 let pencilColor = "blue";
 
-tool.lineWidth = "3";
 
 let mousedown = false;
 
@@ -38,6 +42,16 @@ canvas.addEventListener("mouseup", () => {
   mousedown = false;
 });
 
+downloadBtn.addEventListener("click", ()=>{
+  const url = canvas.toDataURL();
+
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = "board.jpg"
+  a.click();
+})
+
+
 function strokeBegin(strokeObj) {
   tool.beginPath();
   tool.strokeStyle = pencilColor;
@@ -47,3 +61,7 @@ function drawStroke(strokeObj) {
   tool.lineTo(strokeObj.x, strokeObj.y);
   tool.stroke();
 }
+pencilColorsWidth.addEventListener("change", ()=>{
+  tool.lineWidth = pencilColorsWidth.value;
+})
+
